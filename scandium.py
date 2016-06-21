b541e02789165643c9ff7693316ee73a56cf257e
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2016-06-21 21:39:41 +0200

    changed from batch operations to working on one application at a time

--- scandium.py
+++ scandium.py
@@ -1,8 +1,9 @@
 import os
 import glob
 import gzip
+import subprocess
 
-TEMP_DIR = "scandium_apps/"
+WORKDIR = "temp/"
 
 def app_is_system(filename):
     with open(filename) as app_properties:
@@ -11,40 +12,43 @@ def app_is_system(filename):
                 return True
         return False
 
-def unzip_applications(archives):
-    for archive in archives:
-        with gzip.open(archive, 'rb') as g:
+def unzip_application(archive):
+    print("Unzipping %s" %archive)
+    with gzip.open(archive, 'rb') as g:
+        try:
             s = g.read()
+        except IOError:
+            print IOError
 
-        save_path = archive[:-3]
+    save_path = archive[:-3]
 
-        with open(save_path, 'w') as f:
-            f.write(s)
+    with open(save_path, 'w') as f:
+        f.write(s)
 
-def install_applications():
-    for app in glob.glob1(TEMP_DIR, "*.apk"):
-        print("Installing " + app.split("-")[0])
-        os.system("adb install " + app)
-        os.system("rm -f " + app)
+    os.system("rm -f %s" %archive)
+    return save_path
+
+def install_application(app):
+    print("Installing " + app.split("-")[0])
+    os.system("adb install " + app)
+    os.system("rm -f " + app)
+    print("")
 
 def main():
+    os.system("mkdir -p " + WORKDIR)
+    os.chdir(WORKDIR)
+    adb_prop_cp = '''adb shell ls "/mnt/sdcard/TitaniumBackup/*.properties" | tr -d '\r' | xargs -i -n1 adb pull {} .'''
+    adb_ls = ("adb", "shell", "ls", "/mnt/sdcard/TitaniumBackup/")
+    os.system(adb_prop_cp + "> /dev/null 2>&1")
     app_names = [f.split("-")[0] for f in glob.glob1(".", "*.properties") if not app_is_system(f)]
-    archives = [a for a in glob.glob1(".", "*.apk.gz") if a.split("-")[0] in app_names]
-    os.system("mkdir -p " + TEMP_DIR)
+    print("Found %d applications" % len(app_names))
+    p = subprocess.Popen(adb_ls, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    archives = [a for a in p.stdout.read().splitlines() if a.split("-")[0] in app_names and a.endswith(".apk.gz")]
 
-    unzip_applications(archives)
-    install_applications()
-
-    #os.system("rm -rf " + TEMP_DIR)
-
-
-
-
-
-
-
-
-
+    for archive in archives[:3]:
+        os.system("adb pull /mnt/sdcard/TitaniumBackup/%s ." %archive)
+        app = unzip_application(archive)
+        install_application(app)
 
 if __name__ == "__main__":
     main()
