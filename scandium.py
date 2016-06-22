fa2e0f0af85b8ad5ab43003c70443e650cb5be84
Jarle Mathiesen <Jarle.mathiesen@gmail.com>
2016-06-22 13:14:48 +0200

    ported script to windows

--- scandium.py
+++ scandium.py
@@ -2,6 +2,7 @@ import os
 import glob
 import gzip
 import subprocess
+import shutil
 
 WORKDIR = "temp/"
 
@@ -14,34 +15,41 @@ def app_is_system(filename):
 
 def unzip_application(archive):
     print("Unzipping %s" %archive)
+    save_path = archive[:-3]
+    s = open(save_path, 'wb')
     with gzip.open(archive, 'rb') as g:
         try:
-            s = g.read()
+		    shutil.copyfileobj(g, s)
         except IOError:
             print IOError
 
-    save_path = archive[:-3]
-
-    with open(save_path, 'w') as f:
-        f.write(s)
-
-    os.system("rm -f %s" %archive)
+    os.remove(archive)
     return save_path
 
 def install_application(app):
     print("Installing " + app.split("-")[0])
     os.system("adb install " + app)
-    os.system("rm -f " + app)
+    os.remove(app)
     print("")
 
+def get_property_files():
+    adb_prop_ls = ("adb", "shell", "ls", "/mnt/sdcard/TitaniumBackup/*.properties")
+    p = subprocess.Popen(adb_prop_ls, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    property_files = [f for f in p.stdout.read().splitlines() if f.endswith(".properties")]
+    for property_file in property_files:
+	    os.system("adb pull %s" %property_file)
+
 def main():
-    os.system("mkdir -p " + WORKDIR)
+    try:
+    	os.mkdir(WORKDIR)
+    except OSError as e:
+    	print e
+
     os.chdir(WORKDIR)
-    adb_prop_cp = '''adb shell ls "/mnt/sdcard/TitaniumBackup/*.properties" | tr -d '\r' | xargs -i -n1 adb pull {} .'''
-    adb_ls = ("adb", "shell", "ls", "/mnt/sdcard/TitaniumBackup/")
-    os.system(adb_prop_cp + "> /dev/null 2>&1")
+    get_property_files()
     app_names = [f.split("-")[0] for f in glob.glob1(".", "*.properties") if not app_is_system(f)]
     print("Found %d applications" % len(app_names))
+    adb_ls = ("adb", "shell", "ls", "/mnt/sdcard/TitaniumBackup/")
     p = subprocess.Popen(adb_ls, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     archives = [a for a in p.stdout.read().splitlines() if a.split("-")[0] in app_names and a.endswith(".apk.gz")]
 
@@ -49,6 +57,7 @@ def main():
         os.system("adb pull /mnt/sdcard/TitaniumBackup/%s ." %archive)
         app = unzip_application(archive)
         install_application(app)
+        print archive
 
 if __name__ == "__main__":
     main()
